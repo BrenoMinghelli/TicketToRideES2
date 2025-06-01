ae631b57380daaed701f4ce5ac8ffd432067a4d0
Felipe <cacalafrio@gmail.com>
2025-06-01 03:00:41 -0300

    trazendo de volta tickettoridees2/.godot/ e to_lower

--- tickettoridees2/baralho.gd
+++ tickettoridees2/baralho.gd
@@ -12,7 +12,8 @@ func inicializar_baralho():
 	]
 	#seta a cor da carta + a textura, que pode ser ou nao usado futuramente
 	for cor in cores_normais:
-		var nome_cor: String = Carta.Cor.keys()[cor]
+		#var nome_cor: String = Carta.Cor.keys()[cor]
+		var nome_cor: String = Carta.Cor.keys()[cor].to_lower()
 		var textura_path: String = "res://assets/cartas/%s.png" % nome_cor
 		var textura: Texture2D = load(textura_path)
 		#adiciona 12 cartas de cada no baralho
